8b5e7156646832c7049ec304cf842d712843d195
BlebaSbluzu <146071033+BlebaSbluzu@users.noreply.github.com>
2026-04-23 20:34:56 +0100

    4

--- pneumonia_classification.py
+++ pneumonia_classification.py
@@ -1,16 +1,13 @@
-
-
 from __future__ import print_function
 
 import keras
 import tensorflow as tf
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Rescaling, BatchNormalization
+from keras.layers import Dense, Dropout, Flatten,GlobalAveragePooling2D, Conv2D, MaxPooling2D, Rescaling, BatchNormalization,RandomFlip, RandomRotation, RandomZoom
 from keras.optimizers import RMSprop,Adam
 import matplotlib.pyplot as plt
 import numpy as np
-
 
 batch_size = 12
 num_classes = 3
@@ -19,8 +16,24 @@ img_width = 128
 img_height = 128
 img_channels = 3
 fit = True #make fit false if you do not want to train the network again
-train_dir = 'C:\\Users\\SimonMcLoughlin\\Documents\\research\\datasets\\chest_xray\\train'
-test_dir = 'C:\\Users\\SimonMcLoughlin\\Documents\\research\\datasets\\chest_xray\\test'
+train_dir = '/home/bleba/Computer-Vision-Assignment-2/Training/train'
+test_dir = '/home/bleba/Computer-Vision-Assignment-2/Training/test'
+
+import os
+
+
+
+
+
+print("\nTRAIN DISTRIBUTION")
+for cls in ["BACTERIAL", "NORMAL", "VIRAL"]:
+    folder = os.path.join(train_dir, cls)
+    count = len([
+        f for f in os.listdir(folder)
+        if os.path.isfile(os.path.join(folder, f))
+    ])
+    print(cls, count)
+
 
 with tf.device('/gpu:0'):
     
@@ -47,6 +60,15 @@ with tf.device('/gpu:0'):
     print('Class Names: ',class_names)
     num_classes = len(class_names)
     
+    class_weight = {
+    0: 0.70,   # bacterial
+    1: 1.24,   # normal
+    2: 1.33    # viral
+    }
+
+    print("Class weights:", class_weight)
+
+
     plt.figure(figsize=(10, 10))
     for images, labels in train_ds.take(2):
         for i in range(6):
@@ -58,6 +80,11 @@ with tf.device('/gpu:0'):
 
     #create model
     model = tf.keras.models.Sequential([
+
+        RandomFlip("horizontal"),
+        RandomRotation(0.05),
+        RandomZoom(0.1),
+        
         Rescaling(1.0/255),
         Conv2D(16, (3,3), activation = 'relu', input_shape = (img_height,img_width, img_channels)),
         MaxPooling2D(2,2),
@@ -65,9 +92,16 @@ with tf.device('/gpu:0'):
         MaxPooling2D(2,2),
         Conv2D(32, (3,3), activation = 'relu'),
         MaxPooling2D(2,2),
-        Flatten(), # flatten multidimensional outputs into single dimension for input to dense fully connected layers
-        Dense(512, activation = 'relu'),
-        Dropout(0.2),
+        # Flatten(), # flatten multidimensional outputs into single dimension for input to dense fully connected layers
+        # Dense(512, activation = 'relu'),
+        # Dropout(0.2),
+
+        #Helps with overfitting instead cuz it reduces the number of trainable
+        # parameters and prevents the model from memorising the training data.
+        GlobalAveragePooling2D(),
+        Dropout(0.3),
+
+
         Dense(num_classes, activation = 'softmax')
     ])
 
@@ -80,11 +114,12 @@ with tf.device('/gpu:0'):
 
     if fit:
         history = model.fit(
-            train_ds,
-            batch_size=batch_size,
-            validation_data=val_ds,
-            callbacks=[save_callback],
-            epochs=epochs)
+        train_ds,
+        validation_data=val_ds,
+        callbacks=[save_callback],
+        epochs=epochs,
+        class_weight=class_weight
+        )
     else:
         model = tf.keras.models.load_model("pneumonia.keras")
 
@@ -93,6 +128,7 @@ with tf.device('/gpu:0'):
     print('Test accuracy:', score[1])
 
     
+
     if fit:
         plt.plot(history.history['accuracy'])
         plt.plot(history.history['val_accuracy'])
