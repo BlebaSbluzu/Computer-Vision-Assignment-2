fc11eb3728a9747585eecd2a4b2f763736abe3b9
BlebaSbluzu <146071033+BlebaSbluzu@users.noreply.github.com>
2026-04-23 22:10:31 +0100

    Keras Cancer

--- pneumonia_classification.py
+++ pneumonia_classification.py
@@ -8,6 +8,8 @@ from keras.layers import Dense, Dropout, Flatten,GlobalAveragePooling2D, Conv2D,
 from keras.optimizers import RMSprop,Adam
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import keras_tuner as kt
 
 batch_size = 12
 num_classes = 3
@@ -18,11 +20,6 @@ img_channels = 3
 fit = True #make fit false if you do not want to train the network again
 train_dir = '/home/bleba/Computer-Vision-Assignment-2/Training/train'
 test_dir = '/home/bleba/Computer-Vision-Assignment-2/Training/test'
-
-import os
-
-
-
 
 
 print("\nTRAIN DISTRIBUTION")
@@ -78,19 +75,20 @@ with tf.device('/gpu:0'):
             plt.axis("off")
     plt.show()
 
-    #create model
-    model = tf.keras.models.Sequential([
+
+    def model_builder(hp):
+        model = tf.keras.models.Sequential([
 
         RandomFlip("horizontal"),
         RandomRotation(0.05),
         RandomZoom(0.1),
         
         Rescaling(1.0/255),
-        Conv2D(16, (3,3), activation = 'relu', input_shape = (img_height,img_width, img_channels)),
+        Conv2D(hp.Int("conv1_filters", min_value=16, max_value=64, step=16), (3,3), activation = 'relu', input_shape = (img_height,img_width, img_channels)),
         MaxPooling2D(2,2),
-        Conv2D(32, (3,3), activation = 'relu'),
+        Conv2D(hp.Int("conv2_filters", min_value=32, max_value=128, step=32), (3,3), activation = 'relu'),
         MaxPooling2D(2,2),
-        Conv2D(32, (3,3), activation = 'relu'),
+        Conv2D(hp.Int("conv3_filters", min_value=32, max_value=128, step=32), (3,3), activation = 'relu'),
         MaxPooling2D(2,2),
         # Flatten(), # flatten multidimensional outputs into single dimension for input to dense fully connected layers
         # Dense(512, activation = 'relu'),
@@ -99,17 +97,48 @@ with tf.device('/gpu:0'):
         #Helps with overfitting instead cuz it reduces the number of trainable
         # parameters and prevents the model from memorising the training data.
         GlobalAveragePooling2D(),
-        Dropout(0.3),
+        Dropout(hp.Choice("dropout_rate", values=[0.2, 0.3, 0.4, 0.5])),
 
 
         Dense(num_classes, activation = 'softmax')
     ])
 
-    model.compile(loss='sparse_categorical_crossentropy',
-                  optimizer=Adam(),
+        hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+
+        model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=Adam(learning_rate=hp_learning_rate),
                   metrics=['accuracy'])
+
+        return model
+
+
+    tuner = kt.Hyperband(
+        model_builder,
+        objective="val_accuracy",
+        max_epochs=10,
+        factor=3,
+        directory="keras_tuner_dir",
+        project_name="pneumonia_tuning"
+    )
+
+    tuner.search(
+        train_ds,
+        validation_data=val_ds,
+        epochs=10,
+        class_weight=class_weight
+    )
+
+    best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+    print("Best conv1 filters:", best_hps.get("conv1_filters"))
+    print("Best conv2 filters:", best_hps.get("conv2_filters"))
+    print("Best conv3 filters:", best_hps.get("conv3_filters"))
+    print("Best dropout:", best_hps.get("dropout_rate"))
+    print("Best learning rate:", best_hps.get("learning_rate"))
+
+
+    model = tuner.hypermodel.build(best_hps)
     
-    #earlystop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=5)
     save_callback = tf.keras.callbacks.ModelCheckpoint("pneumonia.keras",save_freq='epoch',save_best_only=True)
 
     if fit:
@@ -128,7 +157,6 @@ with tf.device('/gpu:0'):
     print('Test accuracy:', score[1])
 
     
-
     if fit:
         plt.plot(history.history['accuracy'])
         plt.plot(history.history['val_accuracy'])
@@ -143,7 +171,7 @@ with tf.device('/gpu:0'):
         for i in range(6):
             ax = plt.subplot(2, 3, i + 1)
             plt.imshow(images[i].numpy().astype("uint8"))
-            prediction = model.predict(tf.expand_dims(images[i].numpy(),0))#perform a prediction on this image
+            prediction = model.predict(tf.expand_dims(images[i].numpy(),0))
             plt.title('Actual:' + class_names[labels[i].numpy()]+ '\nPredicted:{} {:.2f}%'.format(class_names[np.argmax(prediction)], 100 * np.max(prediction)))
             plt.axis("off")
     plt.show()
